d24ddc9a074d28dc5749d78cfb0bebca72c4a967
Vinu_DevOps <44870110+VinothKumar-DevOps@users.noreply.github.com>
2019-10-24 10:32:01 +0530

    Update mysql-setup.sh

--- mysql-server/mysql-setup.sh
+++ mysql-server/mysql-setup.sh
@@ -1,6 +1,32 @@
 #!/bin/sh
 
 # Keep upstart from complaining
+
+
+FROM ubuntu:bionic-20190612
+
+LABEL maintainer="sameer@damagehead.com"
+
+ENV MYSQL_USER=mysql \
+    MYSQL_VERSION=5.7.26 \
+    MYSQL_DATA_DIR=/var/lib/mysql \
+    MYSQL_RUN_DIR=/run/mysqld \
+    MYSQL_LOG_DIR=/var/log/mysql
+
+RUN apt-get update \
+ && DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server=${MYSQL_VERSION}* \
+ && rm -rf ${MYSQL_DATA_DIR} \
+ && rm -rf /var/lib/apt/lists/*
+
+COPY entrypoint.sh /sbin/entrypoint.sh
+
+RUN chmod 755 /sbin/entrypoint.sh
+
+EXPOSE 3306/tcp
+
+ENTRYPOINT ["/sbin/entrypoint.sh"]
+
+CMD ["/usr/bin/mysqld_safe"]
 RUN dpkg-divert --local --rename --add /sbin/initctl
 RUN ln -s /bin/true /sbin/initctl
 
